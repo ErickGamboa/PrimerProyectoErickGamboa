b78aaa60d99618c2fa7ef8a5f20c2381f7ec1ca3
erick gamboa <erick.yosue@gmail.com>
2020-10-14 17:57:02 -0600

    avance con archivos Json y Login

---
+++ MatriculaUniversitaria/MatriculaUniversitaria/ventanalogin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaEntidades;
+using CapaLogica;
+
+namespace MatriculaUniversitaria
+{
+    public partial class ventanaLogin : Form
+    {
+        public LLogin login;
+        public List<EAdmin> loginA;
+
+        public ventanaLogin()
+        {
+            InitializeComponent();
+            login = new LLogin();
+            loginA = new List<EAdmin>();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loginA = login.cargarDatos();
+            lblError.Text = loginA[1].usuario;
+        }
+
+        private void ventanaLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
