905499c6ebc8107a1e777e674f1da03fe4c805cb
erick gamboa <erick.yosue@gmail.com>
2020-10-19 19:16:11 -0600

    avance crud materia

---
+++ MatriculaUniversitaria/CapaEntidades/Materia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Materia
+    {
+        public string idMateria { get; set; }
+        public string nombreMateria { get; set; }
+        public string creditosMateria { get; set; }
+        public string idCarrera { get; set; }
+        public string precioMateria { get; set; }
+        public string costoMateria { get; set; }
+
+    }
+}
